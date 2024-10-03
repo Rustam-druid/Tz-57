87666503999383314f632c1b4eac4ea666ac613c
Mabo Rustam <kimlael01@mail.ru>
2024-10-03 12:26:01 +0600

    App init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Plovo from "./conteiners/Plovo/Plovo.tsx";
+
+const App = () => {
+  return (
+    <>
+      <Plovo />
+    </>
+  );
+};
+
+export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export interface IDish {
+  id: string;
+  name: string;
+email: string;
+category:string
+}
+
+export interface IDishMutation {
+  name: string;
+email: string;
+  urlImage: string;
+  category:string
+}
